b06c3da8a142d9ec0d4800ae519c58b3c8a3e741
Francisco Bernal Yescas <francisco@sproutling.com>
2017-06-20 23:03:14 -0700

    DB

--- index.js
+++ index.js
@@ -23,27 +23,24 @@ app.get('/films', function (req, res) {
   //ask for a client from the pool
   db.connect(function (err, client) {
     // Handle connection errors
-    if(err) {
+    if (err) {
       console.log(err);
-      return res.status(500).json({success: false, data: err});
+      return res.status(500).json({
+        success: false,
+        data: err
+      });
     }
-    // SQL Query > Select Data
-//    const query = client.query('SELECT * FROM public.user;');
-//    // Stream results back one row at a time
-//    query.on('row', (row) => {
-//      results.push(row);
-//    });
-//    // After all data is returned, close connection and return results
-//    query.on('end', () => {
-//      done();
-//      return res.json(results);
-//    });
-    
-    res.send('Connected');
+    client.query('SELECT * FROM public.film', function (err, result) {
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+        response.render('pages/db', {
+          results: result.rows
+        });
+      }
+    });
   });
-
-
-
 });
 
 app.listen(app.get('port'), function () {

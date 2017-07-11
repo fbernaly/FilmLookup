e6c662bbeee87e1193ddf8500c511e2e6be67b76
Francisco Bernal Yescas <francisco@sproutling.com>
2017-07-11 02:22:07 -0700

    using passport

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const api = require('./routes/api/api');
 const pages = require('./routes/pages/pages');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
 
 // set port
 app.set('port', (process.env.PORT || 5000));
@@ -11,18 +14,37 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// set session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// flash is a special area of the session used for storing messages
+app.use(flash());
+
 // public directory
 app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 // parse application/json
 app.use(bodyParser.json())
 
+// initialize Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
 // routers
 app.use('/api', api);
 app.use('/', pages);
+
+// set authentication
+require('./authentication').init(app);
 
 // start server
 app.listen(app.get('port'), function () {

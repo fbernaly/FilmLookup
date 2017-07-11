76b7f3630f1042c50cb9eb40900667c161ae60ea
Francisco Bernal Yescas <francisco@sproutling.com>
2017-07-11 14:39:21 -0700

    update role

--- public/js/update.js
+++ public/js/update.js
@@ -1,2 +1,12 @@
 function updateRole(id, admin) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+      if (xmlhttp.status == 200) {} else {}
+    }
+  };
+  xmlhttp.open('PUT', 'api/users/' + id);
+  xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  var role = admin == true ? "2" : "1";
+  xmlhttp.send('role_id=' + role);
 }
